23dce53a6d7b6d7ab9f4e48ea9a157443f4d8910
Nicolas Cieri <nicolas.cieri@vnstudios.com>
2017-07-23 21:13:16 -0300

    home

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -19,8 +19,20 @@ class Parallax {
 
   renderParallax() {
     window.requestAnimationFrame(() => {
-      let position = document.body.getBoundingClientRect().top * .3 * (-1);
-      this.bkg.style.transform = "translate3d(-50%, " + position.toFixed(0) + "px, 0px)";
+      // let bodyScroll = document.body.getBoundingClientRect().top * (-1);
+      let elemPosition = this.elem.getBoundingClientRect().top;
+      let difference = window.innerHeight - elemPosition;
+      if(difference >= 0) {
+        var bkgPosition = difference * 0.3;
+        this.bkg.style.transform = "translate3d(-50%, " + bkgPosition.toFixed(0) + "px, 0px)";
+      }
+
+      // let isElementVisible = window.innerHeight > elemPosition;
+
+      // let position = document.body.getBoundingClientRect().top * .3 * (-1);
+      // let position = window.innerHeight - elemPosition;
+      // console.log(position)
+      // this.bkg.style.transform = "translate3d(-50%, " + position.toFixed(0) + "px, 0px)";
     });
   }
 
