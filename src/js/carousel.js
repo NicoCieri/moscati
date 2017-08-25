28e44672ecaa9b6fc8e6e1bb2d6bb79bbea0e385
Nicolas Cieri <nico.cieri13@hotmail.com>
2017-08-25 00:45:34 -0300

    fix carousel when not found

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -3,18 +3,20 @@ import Flickity from 'flickity-imagesloaded';
 class Carousel {
   constructor(selector, settings={}) {
     this.elem = $(selector);
-    this.fullpageBtn = this.elem.find('.full-screen-btn');
-    this.carousel = new Flickity(selector, {
-      contain: true,
-      cellSelector: '.carousel-cell',
-      imagesLoaded: true,
-      cellAlign: 'center',
-      wrapAround: true,
-      autoPlay: true,
-      autoPlay: 5000,
-      ...settings
-    });
-    this.registerListeners();
+    if(this.elem.length) {
+      this.fullpageBtn = this.elem.find('.full-screen-btn');
+      this.carousel = new Flickity(selector, {
+        contain: true,
+        cellSelector: '.carousel-cell',
+        imagesLoaded: true,
+        cellAlign: 'center',
+        wrapAround: true,
+        autoPlay: true,
+        autoPlay: 5000,
+        ...settings
+      });
+      this.registerListeners();
+    }
   }
 
   registerListeners() {
